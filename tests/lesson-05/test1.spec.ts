c0231bee39526330bca5ecab0df724ad6398ee91
maiphan1509 <phanthithanhmai1509@gmail.com>
2026-03-16 20:23:27 +0700

    feat(lesson-05): fix rate star

--- tests/lesson-05/test1.spec.ts
+++ tests/lesson-05/test1.spec.ts
@@ -1,13 +1,13 @@
-import {expect, test} from '@playwright/test';
+import { expect, Page, test } from "@playwright/test";
 
-test('Register Page Test', async ({page}) => {
-  await page.goto('https://material.playwrightvn.com/');
+test("Register Page Test", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/");
 
-  await page.click('text=Bài học 1: Register Page');
+  await page.click("text=Bài học 1: Register Page");
 
-  await page.locator('input[id="username"]').fill('Mai Phan');
+  await page.locator('input[id="username"]').fill("Mai Phan");
 
-  await page.locator('input[id="email"]').fill('mai.phan@example.com');
+  await page.locator('input[id="email"]').fill("mai.phan@example.com");
 
   await page.locator('input[id="female"]').click();
 
@@ -15,31 +15,47 @@ test('Register Page Test', async ({page}) => {
   await page.locator('input[id="cooking"]').click();
   await page.locator('input[id="traveling"]').click();
 
-  await page.locator('select[id="interests"]').selectOption('art');
+  await page.locator('select[id="interests"]').selectOption("art");
 
-  await page.locator('select[id="country"]').selectOption('uk');
+  await page.locator('select[id="country"]').selectOption("uk");
 
-  await page.locator('input[id="dob"]').fill('1999-09-15');
+  await page.locator('input[id="dob"]').fill("1999-09-15");
 
-  await page.locator('input[id="profile"]').setInputFiles('tests/lesson-05/01-dom.txt');
+  await page
+    .locator('input[id="profile"]')
+    .setInputFiles("tests/lesson-05/01-dom.txt");
 
-  await page.locator('textarea[id="bio"]').fill('This is a test message.');
+  await page.locator('textarea[id="bio"]').fill("This is a test message.");
 
-  await page.locator('input[id="rating"]').fill('4');
+  await page.locator('input[id="rating"]').fill("4");
 
-  await page.locator('input[type="color"]').fill('#000604');
+  await page.locator('input[type="color"]').fill("#000604");
 
   await page.locator('div[class="tooltip"]').hover();
   await page.locator('input[id="newsletter"]').click();
 
   await page.locator('label[class="switch"]').click();
 
-  await page.locator('div[id="starRating"]').evaluate((element) => {
-    const stars = element.setAttribute('data-stars', '4.5');
-  });
+  await rateStar(page, 4.5);
 
   await page.locator('button[type="submit"]').click();
 
-  await expect(page.locator('#userTable tbody tr')).toHaveCount(1);
+  await expect(page.locator("#userTable tbody tr")).toHaveCount(1);
+});
 
-})
+const rateStar = async (page: Page, rating: number) => {
+  const size = await startRatingSize(page);
+  console.log(size);
+  const clickX = (rating / 5) * size.width;
+  const clickY = size.height / 2;
+
+  await page
+    .locator(`div[id="starRating"]`)
+    .click({ position: { x: clickX, y: clickY } });
+};
+
+const startRatingSize = async (page: Page) => {
+  return await page.locator(`div[id="starRating"]`).evaluate((el) => {
+    return { width: el.clientWidth, height: el.clientHeight };
+  });
+};
